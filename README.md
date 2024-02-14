d3e8177dd0bc2c63f07fbb17cc01b30894852cf0
baniramdlan <baniramdlan@gmail.com>
2024-02-14 11:25:57 +0700

    ## 0.0.1
    * Add GroupItem Class

---
+++ README.md
@@ -0,0 +1,92 @@
+GroupItem Widget
+
+## Usage
+
+```dart
+import 'package:devaloop_group_item/group_tem.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Group Item Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Group Item Example'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              GroupItem(
+                title: 'Transaction',
+                contents: [
+                  GroupContent(
+                    title: 'Sell',
+                    subtitle: 'Sell',
+                    leading: const Icon(Icons.sell),
+                  ),
+                  GroupContent(
+                    title: 'Buy',
+                    subtitle: 'Buy',
+                    leading: const Icon(Icons.shop),
+                  ),
+                ],
+              ),
+              GroupItem(
+                title: 'Inventory',
+                contents: [
+                  GroupContent(
+                    title: 'Inventory',
+                    subtitle: 'Inventory',
+                    leading: const Icon(Icons.inventory),
+                  ),
+                ],
+              ),
+              GroupItem(
+                title: 'Reporting',
+                contents: [
+                  GroupContent(
+                    title: 'Reporting',
+                    subtitle: 'Reporting',
+                    leading: const Icon(Icons.summarize),
+                    detail: Detail(
+                      detailPage: const ReportingPage(),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ReportingPage extends StatelessWidget {
+  const ReportingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Reporting'),
+      ),
+      body: const Center(
+        child: Text('Content'),
+      ),
+    );
+  }
+}
+```
---
+++ lib/group_tem.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class GroupItem extends StatelessWidget {
+  final String? title;
+  final List<GroupContent> contents;
+
+  const GroupItem({
+    super.key,
+    this.title,
+    required this.contents,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7.5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          if (title != null)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Text(title!.toUpperCase()),
+            ),
+          Card(
+            child: ListView.separated(
+              shrinkWrap: true,
+              itemBuilder: (context, index) => ListTile(
+                leading: contents[index].leading,
+                title: Text(contents[index].title),
+                subtitle: Text(contents[index].subtitle),
+                trailing: contents[index].trailing,
+                onTap: contents[index].detail == null
+                    ? null
+                    : () async {
+                        var result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                contents[index].detail!.detailPage,
+                          ),
+                        );
+
+                        if (contents[index].detail!.onDetailPageClosed !=
+                            null) {
+                          await contents[index]
+                              .detail!
+                              .onDetailPageClosed!
+                              .call(result);
+                        }
+                      },
+              ),
+              separatorBuilder: (context, index) => const Divider(),
+              itemCount: contents.length,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class GroupContent {
+  final Widget? leading;
+  final String title;
+  final String subtitle;
+  final Widget? trailing;
+  final Detail? detail;
+
+  GroupContent({
+    this.leading,
+    required this.title,
+    required this.subtitle,
+    this.trailing,
+    this.detail,
+  });
+}
+
+class Detail {
+  final Widget detailPage;
+  final dynamic Function(dynamic result)? onDetailPageClosed;
+
+  Detail({required this.detailPage, this.onDetailPageClosed});
+}
